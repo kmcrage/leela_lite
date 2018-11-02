f5bf35db6db858514d83397d500ec89017a72066
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-02 18:50:47 -0400

    mcts-voi imlementation

--- search/__init__.py
+++ search/__init__.py
@@ -2,4 +2,5 @@ from search.neural_net import NeuralNet
 from search.uct import UCT_search
 from search.crazy import CRAZY_search
 from search.brue import BRUE_search
+from search.voi import VOI_search
 # from uct.util import softmax, temp_softmax
