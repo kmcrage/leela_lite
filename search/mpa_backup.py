eb58e37d2b7de27b8220ad06c8ea2396761004f6
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-03 22:05:59 -0400

    replace total_value with q

--- search/mpa_backup.py
+++ search/mpa_backup.py
@@ -20,14 +20,14 @@ class MPANode():
         return (math.sqrt(self.parent.number_visits)
                 * self.prior / (1 + self.number_visits))
 
-    def best_child(self, C):
+    def best_child(self, c):
         return max(self.children.values(),
-                   key=lambda node: node.Q + C*node.U())
+                   key=lambda node: node.Q + c*node.U())
 
-    def select_leaf(self, C):
+    def select_leaf(self, c):
         current = self
         while current.is_expanded and current.children:
-            current = current.best_child(C)
+            current = current.best_child(c)
         if not current.board:
             current.board = current.parent.board.copy()
             current.board.push_uci(current.move)
@@ -47,12 +47,13 @@ class MPANode():
         self.number_visits += 1
         while current.parent is not None:
             current = current.parent
-            print('preupdate Q:', current.Q, len(current.children), current.number_visits)
+            # print('preupdate Q:', current.Q, len(current.children), current.number_visits)
             current.number_visits += 1
             current.Q = 0
             visits = 0
             for child in [n for n in current.children.values() if n.number_visits]:
-                print('child q', child.Q, child.number_visits, [n.Q for n in child.children.values() if n.number_visits])
+                # print('child q', child.Q, child.number_visits,
+                # [n.Q for n in child.children.values() if n.number_visits])
                 qs = [n.Q for n in child.children.values() if n.number_visits]
                 if qs:
                     current.Q += child.number_visits * max(qs)
@@ -60,26 +61,26 @@ class MPANode():
                 else:
                     current.Q -= child.number_visits * child.Q
                     visits += child.number_visits
-                print('updating Q:', current.Q, current.number_visits)
+                # print('updating Q:', current.Q, current.number_visits)
             if visits:
                 current.Q /= visits
-            print('postupdate Q:', current.Q, current.number_visits)
+            # print('postupdate Q:', current.Q, current.number_visits)
 
     def dump(self, move, C):
         print("---")
         print("move: ", move)
-        #print("total value: ", self.total_value)
+        # print("total value: ", self.total_value)
         print("visits: ", self.number_visits)
         print("prior: ", self.prior)
         print("Q: ", self.Q)
         print("U: ", self.U())
         print("BestMove: ", self.Q + C * self.U())
-        #print("math.sqrt({}) * {} / (1 + {}))".format(self.parent.number_visits,
+        # print("math.sqrt({}) * {} / (1 + {}))".format(self.parent.number_visits,
         #      self.prior, self.number_visits))
         print("---")
 
 def MPA_search(board, num_reads, net=None, C=1.0):
-    assert(net != None)
+    assert(net is not None)
     root = MPANode(board)
     root.number_visits = 1
     for _ in range(num_reads):
@@ -95,14 +96,3 @@ def MPA_search(board, num_reads, net=None, C=1.0):
     print('MPA pv:', [(n[0], n[1].Q, n[1].number_visits) for n in pv])
     return max(root.children.items(),
                key=lambda item: (item[1].number_visits, item[1].Q))
-
-
-
-#num_reads = 10000
-#import time
-#tick = time.time()
-#UCT_search(GameState(), num_reads)
-#tock = time.time()
-#print("Took %s sec to run %s times" % (tock - tick, num_reads))
-#import resource
-#print("Consumed %sB memory" % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
