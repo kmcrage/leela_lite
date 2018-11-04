e2c72f8b3085d2ab977663346727579ed0378c3e
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-03 22:32:35 -0400

    uci engines

--- leelalite.sh
+++ leelalite.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-DIR="/home/dkappe/work/src/leela_lite"
-
-. ~/envs/lcztools/bin/activate
-
-cd $DIR
-exec python engine.py weights_9149.txt.gz 800
+exec python3 /content/leela_lite/engine.py pytorch_cpu /content/leela-chess-weights/weights/weights_125.txt.gz 800 3.4
---
+++ leelalite_mpa.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec python3 /content/leela_lite/engine_mpa.py pytorch_cpu /content/leela-chess-weights/weights/weights_125.txt.gz 800 3.4
