77caea94c6b66a2abc75e6764df7fda2546665ee
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-06 19:13:14 -0500

    generalis self-play script

--- leela_lite.py
+++ leela_lite.py
@@ -8,62 +8,41 @@ import chess.pgn
 import time
 from os import path
 
+engines = {'uct': search.UCT_search,
+           'minmax': search.MinMax_search
+           'bellman': search.Bellman_search
+           'mpa': search.MPA_search}
+
 if len(sys.argv) != 4:
-    print("Usage: python3 leela_lite.py <weights file> <nodes> <c>")
+    print("Usage: python3 leela_lite.py <policy1> <policy2> <weights file> <nodes>")
     print(len(sys.argv))
     exit(1)
 
+players = [sys.argv[1], sys.argv[2]]
+weights = sys.argv[3]
+nodes = int(sys.argv[4])
+c = float(sys.argv[5])
+
+
 backend = 'pytorch_cuda' if path.exists('/opt/bin/nvidia-smi') else 'pytorch_cpu'
-weights = sys.argv[1]
-nodes = int(sys.argv[2])
-c = float(sys.argv[3])
-
-
 board = LeelaBoard()
-
 net = load_network(backend=backend, filename=weights, policy_softmax_temp=2.2)
 nn = search.NeuralNet(net=net)
-# policy, value = net.evaluate(board)
-# print(policy)
-# print(value)
-# print(uct.softmax(policy.values()))
 
-SELFPLAY = True
 
+turn = 0
 while True:
-    if not SELFPLAY:
-        print(board)
-        print("Enter move: ", end='')
-        sys.stdout.flush()
-        line = sys.stdin.readline()
-        line = line.rstrip()
-        board.push_uci(line)
     print(board)
     print("thinking...")
     start = time.time()
-    best, node = search.UCT_search(board, nodes, net=nn, C=c)
+    best, node = engines[players[turn]](board, nodes, net=nn, C=c)
     elapsed = time.time() - start
-    print(board.pc_board.fullmove_number, "UCT best: ", best, node.Q())
+    print(board.pc_board.fullmove_number, players[turn], "best: ", best)
     print("Time: {:.3f} nps".format(nodes/elapsed))
-    #print(nn.evaluate.cache_info())
     board.push_uci(best)
     if board.pc_board.is_game_over() or board.is_draw():
         print("Game over... result is {}".format(board.pc_board.result(claim_draw=True)))
         print(board)
         print(chess.pgn.Game.from_board(board.pc_board))
         break
-    print(board)
-    print("thinking...")
-    start = time.time()
-    best, node = search.MinMax_search(board, nodes, net=nn, C=c)
-    elapsed = time.time() - start
-    print(board.pc_board.fullmove_number, "MinMax best: ", best, node.Q)
-    print("Time: {:.3f} nps".format(nodes/elapsed))
-    #print(nn.evaluate.cache_info())
-    board.push_uci(best)
-    if board.pc_board.is_game_over() or board.is_draw():
-        print("Game over... result is {}".format(board.pc_board.result(claim_draw=True)))
-        print(board)
-        print(chess.pgn.Game.from_board(board.pc_board))
-        break
-
+    turn = 1 - turn
