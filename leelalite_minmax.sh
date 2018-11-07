21c48c2c6586933d518258e21dc7aec0f71eee50
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-06 20:09:51 -0500

    shell calls wrong search

--- leelalite_minmax.sh
+++ leelalite_minmax.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec python3 /content/leela_lite/engine.py /content/leela-chess-weights/weights/weights_125.txt.gz 800
+exec python3 /content/leela_lite/engine_minmax.py /content/leela-chess-weights/weights/weights_125.txt.gz 800
