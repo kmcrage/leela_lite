6d2a90c29811a076b39676f11ea341a7406c0e42
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-07 22:37:45 -0500

    srcr test

--- search/srcr.py
+++ search/srcr.py
@@ -102,6 +102,6 @@ def SRCR_search(board, num_reads, net=None, C_sr=3.4, C_cr=3.4):
                         key=lambda item: (item[1].number_visits, item[1].Q()))
 
     print('SRCR (', C_sr, ',', C_cr, ') pv:', [(n[0], n[1].Q(), n[1].number_visits,
-                                          n[1].U_cr(), n[1].U_sr()) for n in pv])
+                                          C_sr*n[1].U_sr(), C_cr*n[1].U_cr()) for n in pv])
     return max(root.children.items(),
                key=lambda item: (item[1].number_visits, item[1].Q()))
