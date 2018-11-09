43631f0927fa0903bf6f3daf2a5533653c512880
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-09 18:34:04 -0500

    uct variance

--- search/uctv.py
+++ search/uctv.py
@@ -105,8 +105,7 @@ def UCTV_search(board, num_reads, net=None, C=1.0, zeta=1.0):
 
 
     print('UCTV pv:', [(n[0], n[1].Q(), n[1].number_visits, n[1].sigma()) for n in pv])
-    return max(root.children.items(),
-               key=lambda item: (item[1].number_visits, item[1].Q()))
+    return pv[0]
 
 
 
