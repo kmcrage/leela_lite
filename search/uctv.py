0bf5739e2fe627806cdca3f80424766201aa11f0
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-09 19:00:05 -0500

    uct variance

--- search/uctv.py
+++ search/uctv.py
@@ -96,12 +96,12 @@ def UCTV_search(board, num_reads, net=None, C=1.0, zeta=1.0):
     # Here, we are explicitly sampling the variance, and many samples are to reduce
     # variance, so picking the natural max makes more sense
     size = min(5, len(root.children))
-    pv = heapq.nlargest(size, root.children.items(),
-                        key=lambda item: (item[1].number_visits !=0, item[1].Q(), item[1].number_visits))
+    #pv = heapq.nlargest(size, root.children.items(),
+    #                    key=lambda item: (item[1].number_visits !=0, item[1].Q(), item[1].number_visits))
 
     # robust max,
-    # pv = heapq.nlargest(size, root.children.items(),
-    #                    key=lambda item: (item[1].number_visits, item[1].Q())
+    pv = heapq.nlargest(size, root.children.items(),
+                        key=lambda item: (item[1].number_visits, item[1].Q())
 
 
     print('UCTV pv:', [(n[0], n[1].Q(), n[1].number_visits, n[1].sigma()) for n in pv])
