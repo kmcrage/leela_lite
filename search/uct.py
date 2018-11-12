16a75e3bb8134a98cac28cd2f516031e5f9f7021
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-11 19:30:06 -0500

    signs again in the backup procedure

--- search/uct.py
+++ search/uct.py
@@ -108,7 +108,7 @@ class UCTNode:
         print('prediction:', next[0], end=' ')
         while len(next[1].children):
             next = heapq.nlargest(1, next[1].children.items(),
-                                    key=lambda item: (item[1].number_visits, item[1].Q(alpha)))[0]
+                                    key=lambda item: (item[1].number_visits, item[1].Q()))[0]
             print(next[0], end=' ')
         print('')
 
