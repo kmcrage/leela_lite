9f5b8a337c196dd7821793cc1fc66e7e123dddc1
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-14 20:58:17 -0500

    ucd implemetation

--- search/ucd.py
+++ search/ucd.py
@@ -17,7 +17,7 @@ class UCDRollout:
         self.root = root
 
     def leaf_node(self):
-        print(self, self.history)
+        print(self.root, self.history)
         if self.history:
             node = self.history[-1].child
         else:
