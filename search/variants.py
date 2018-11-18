19059ee128bb3fec0ae1fc513e828e86c9195a3b
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-17 20:29:37 -0500

    nonlinear sequential elimination

--- search/variants.py
+++ search/variants.py
@@ -9,13 +9,14 @@ class Cutoff_mixin:
     def select_leaf(self):
         current = self
         min_reward = current.reward - self.cutoff
-        max_reward = current.reward - self.cutoff
+        max_reward = current.reward + self.cutoff
         while current.is_expanded and current.children and min_reward < current.reward < max_reward:
             current = current.best_child()
         if not current.board:
             current.board = current.parent.board.copy()
             current.board.push_uci(current.move)
         return current
+
 
 class CutoffUCTNode(Cutoff_mixin, UCTNode):
     name = 'cutoff'
