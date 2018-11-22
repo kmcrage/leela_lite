167db67e651a57d09e4473313122a2c03f13d6f4
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-22 00:27:26 -0500

    mcts alphabeta

--- search/alphabeta.py
+++ search/alphabeta.py
@@ -36,20 +36,20 @@ class ABNode:
         alpha = self.alpha
         beta = self.beta
         current = self
-        print('selct leaf', d, alpha, beta)
+        # print('selct leaf', d, alpha, beta)
         while current.is_expanded and current.children and d:
             feasible_children = []
             for child in current.children:
                 child.alpha = max(alpha, child.v_minus[d-1])
                 child.beta = min(beta, child.v_plus[d-1])
-                print('child', child.move, child.v_minus[d-1], child.v_plus[d-1], child.alpha, child.beta)
+                # print('child', child.move, child.v_minus[d-1], child.v_plus[d-1], child.alpha, child.beta)
                 if child.alpha < child.beta:
                     feasible_children.append(child)
             current = feasible_children[0]
             d -= 1
             alpha = -current.beta
             beta = -current.alpha
-            print('selcting', [c.move for c in feasible_children])
+            # print('selcting', [c.move for c in feasible_children])
         if not current.board:
             current.board = current.parent.board.copy()
             current.board.push_uci(current.move)
@@ -57,7 +57,7 @@ class ABNode:
 
     def expand(self, child_priors):
         self.is_expanded = True
-        #print('child priors', child_priors)
+        # print('child priors', child_priors)
         for move, prior in (list(child_priors.items()))[:self.k]:
             self.add_child(move, prior)
 
@@ -69,13 +69,13 @@ class ABNode:
         d = 0
         current.v_plus[d] = value_estimate
         current.v_minus[d] = value_estimate
-        print('backup', current.move, value_estimate)
+        # print('backup', current.move, value_estimate)
         while current.parent is not None:
             current = current.parent
             d += 1
             current.v_minus[d] = -min([c.v_plus[d-1] for c in current.children])
             current.v_plus[d] = -min([c.v_minus[d-1] for c in current.children])
-            print('est', current.depth, current.move, current.v_minus[current.depth], current.v_plus[current.depth])
+            # print('est', current.depth, current.move, current.v_minus[current.depth], current.v_plus[current.depth])
         if current.v_minus[current.depth] > current.v_plus[current.depth] - TOLERANCE:
             current.best_child = current.get_best_child(d)
             current.depth += 1
@@ -83,15 +83,15 @@ class ABNode:
     def get_best_child(self, d):
         best_child = self
         v = self.v_plus[d]
-        print('best child', d, best_child.move, v, self.v_plus)
+        # print('best child', d, best_child.move, v, self.v_plus)
         while d:
             v *= -1
             d -= 1
             for c in best_child.children:
-                print('vplus', c.move, d, v, c.v_plus)
+                # print('vplus', c.move, d, v, c.v_plus)
                 if math.fabs(v - c.v_plus[d]) < TOLERANCE:
                     best_child = c
-            print('best child', d, best_child.move)
+            # print('best child', d, best_child.move)
         return best_child
 
     def get_node(self, move):
