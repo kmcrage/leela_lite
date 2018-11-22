cd6706c1591145a03b8390b65c6fde4c8f874622
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-22 00:15:04 -0500

    mcts alphabeta

--- search/alphabeta.py
+++ search/alphabeta.py
@@ -84,7 +84,7 @@ class ABNode:
         best_child = self
         v = self.v_plus[d]
         print('best child', d, best_child.move, self.v_plus)
-        while d > 1:
+        while d:
             d -= 1
             best_child = [c for c in best_child.children if c.v_plus[d] == v][0]
             print('best child', d, best_child.move)
