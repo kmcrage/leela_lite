39591b45ea521515074437b3433ea40a04d98502
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-25 21:25:02 -0500

    thompson

--- search/thompson.py
+++ search/thompson.py
@@ -30,6 +30,24 @@ class Thompson_mixin:
             current.num_wins += self.result_weight * (1 - value_estimate * turnfactor)
             current.num_losses += self.result_weight * (1 + value_estimate * turnfactor)
 
+    def outcome(self):
+        size = min(5, len(self.children))
+        pv = heapq.nlargest(size, self.children.items(),
+                            key=lambda item: (item[1].number_visits, item[1].Q()))
+        if self.verbose:
+            print(self.name, 'pv:', [(n[0],
+                                      n[1].num_wins/(n[1].num_wins+n[1].num_losses),
+                                      n[1].number_visits) for n in pv])
+        # there could be no moves if we jump into a mate somehow
+        print('prediction:', end=' ')
+        predict = pv[0]
+        while len(predict[1].children):
+            predict = heapq.nlargest(1, predict[1].children.items(),
+                                     key=lambda item: (item[1].number_visits, item[1].Q()))[0]
+            print(predict[0], end=' ')
+        print('')
+        return pv[0] if pv else None
+
 
 class UCTTNode(Thompson_mixin, UCTNode):
     name = 'uctt'
