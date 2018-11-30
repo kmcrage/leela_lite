0796098373acf334d44bf11bf775ca6ddba7c809
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-30 06:44:06 -0500

    thompson revisisted

--- search/thompson.py
+++ search/thompson.py
@@ -85,14 +85,12 @@ class Thompson_mixin:
     def outcome(self):
         size = min(5, len(self.children))
         pv = heapq.nlargest(size, self.children.items(),
-                            key=lambda n: (n[1].number_visits,
-                                           n[1].Q()))
+                            key=lambda n: (n[1].Q(),
+                                           n[1].number_visits))
         if self.verbose:
             print(self.name, 'pv:', [(n[0],
                                       n[1].Q(),
                                       .5 + n[1].Q()/2.,
-                                      numpy.random.beta(1 + n[1].prior_wins + n[1].num_wins,
-                                                        1 + n[1].prior_losses + n[1].num_losses),
                                       n[1].num_wins + n[1].num_losses,
                                       n[1].number_visits) for n in pv])
             # there could be no moves if we jump into a mate somehow
