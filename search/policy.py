ea69de7acb87660871dd22be2e46584e67695ad7
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-12-04 18:56:37 -0500

    adaptive policy

--- search/policy.py
+++ search/policy.py
@@ -30,11 +30,9 @@ class Policy_mixin:
         # Child nodes are multiplied by -1 because we want max(-opponent eval)
         turnfactor = -1
         while current:
-            current.number_visits += 1
-
             # update policy before we update the mean (could be simpler just to set policy)
             current.policy *= math.exp((value_estimate * turnfactor - current.Q()) /
-                                       (current.number_visits * current.temperature))
+                                       ((1 + current.number_visits) * current.temperature))
 
             # ... and renormalise the children (we don't care about normalising root)
             if current.children:
@@ -42,6 +40,8 @@ class Policy_mixin:
                 for c in current.children.values():
                     c.policy /= renorm
 
+            # update the mean of Q
+            current.number_visits += 1
             current.total_value += value_estimate * turnfactor
 
             current = current.parent
@@ -56,11 +56,11 @@ class PolicyMinusNode(PolicyUCTNode):
     name = 'policyminus'
 
     def __init__(self, **kwargs):
-        super().__init__(temperature=1.5, **kwargs)
+        super().__init__(temperature=2.5, **kwargs)
 
 
 class PolicyPlusNode(PolicyUCTNode):
     name = 'policyplus'
 
     def __init__(self, **kwargs):
-        super().__init__(temperature=4, **kwargs)
+        super().__init__(temperature=3.5, **kwargs)
